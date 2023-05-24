903e7242f07c931a2505c9928edb211ac1725e51
Buenobarbie <barbarabueno140104@gmail.com>
2023-05-24 09:12:50 -0300

    Datapath prototype

--- src/modules/processor/imm_gen.v
+++ src/modules/processor/imm_gen.v
@@ -3,7 +3,7 @@ module imm_gen #(
     parameter WORDSIZE = 64,            /* define o tamanho da palavra */
     parameter INSTRUCTION_SIZE = 32     /* tamanho da instrução (32 para o RISC-V) */
 ) (
-    input wire [INSTRUCTION_SIZE-1:0] instruction,  /* instrução em 12 bits */
+    input wire [INSTRUCTION_SIZE-1:0] instruction,  /* instrução em 32 bits */
     input wire [WORDSIZE-1:0] immediate             /* immediate em 64 bits */
 );
 
